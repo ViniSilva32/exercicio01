a321dfde8b639f00db4a750c56519e6e7b62f1d9
ViniSilva32 <78766084+ViniSilva32@users.noreply.github.com>
2021-03-04 22:45:24 -0300

    Update roleta! Exercicio 2

--- app/src/main/java/com/example/exercicio01/MainActivity.java
+++ app/src/main/java/com/example/exercicio01/MainActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.EditText;
 
 import java.util.Random;
 
@@ -14,14 +16,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView parabens = findViewById(R.id.imageView);
+        parabens.setVisibility(View.INVISIBLE);
     }
 
     public void rodar(View view) {
+        ImageView parabens = findViewById(R.id.imageView);
 
         TextView texto = findViewById(R.id.numeroSorteado);
 
-        int numero = new Random().nextInt(50);
+        int numero = new Random().nextInt(11);
 
-        texto.setText("numero: " + numero);
+        EditText teste = findViewById(R.id.editTextTextPersonName);
+        int teste4 = Integer.parseInt(teste.getText().toString());
+        if (teste4 == numero){
+            texto.setText("Parabéns, você ganhou!!!");
+            parabens.setVisibility(View.VISIBLE);
+        }
+        else {
+            texto.setText("Infelizmente os números não são iguais!");
+        }
+
     }
 }
